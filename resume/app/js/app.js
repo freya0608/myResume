3d68e6167783bc9da765f4862b391c0f9b4d90e0
freya <985976996@qq.com>
2016-06-27 23:30:15 +0800

    myResume

---
+++ resume/app/js/app.js
@@ -0,0 +1,20 @@
+var bookStoreApp = angular.module('bookStoreApp', [
+    'ngRoute', 'ngAnimate', 'bookStoreCtrls', 'bookStoreFilters',
+    'bookStoreServices', 'bookStoreDirectives'
+]);
+
+bookStoreApp.config(function($routeProvider) {
+    $routeProvider.when('/hello', {
+        templateUrl: 'tpls/hello.html',
+        controller: 'HelloCtrl'
+    }).when('/list', {
+        templateUrl: 'tpls/bookList.html',
+        controller: 'BookListCtrl'
+    }).when('/skill', {
+        templateUrl: 'tpls/mySkill.html',
+        controller: 'MySkillCtrl'
+    })
+        .otherwise({
+        redirectTo: '/hello'
+    })
+});
